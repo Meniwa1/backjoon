54c8176c03474c79e3b2362413f44d10cfc441f6
Meniwa1 <dmsgh1997@gmail.com>
2021-09-30 19:50:33 +0900

    입출력

---
+++ input/BJ_2741.py
@@ -0,0 +1,6 @@
+a = input()
+a = int(a)
+for b in range(1,10):
+    print(a, "X", b, "=",a*b)
+
+
---
+++ input/BJ_8393.py
@@ -0,0 +1,6 @@
+a = input()
+a = int(a)
+sum = 0
+for i in range(0,a+1):
+    sum = sum + i
+print(sum)
